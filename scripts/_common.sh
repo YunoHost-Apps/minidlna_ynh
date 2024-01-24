ff4797a659fe6381907ed5808486f9a64f9d6549
Félix Piédallu <felix@piedallu.me>
2024-01-24 18:29:11 +0100

    Cleanup

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -8,13 +8,12 @@
 # PACKAGE CHECK BYPASSING...
 #=================================================
 
-IS_PACKAGE_CHECK () {
-	if [ ${PACKAGE_CHECK_EXEC:-0} -eq 1 ]
-	then
-		return 0
-	else
-		return 1
-	fi
+IS_PACKAGE_CHECK() {
+    if [ "${PACKAGE_CHECK_EXEC:-0}" -eq 1 ]; then
+        return 0
+    else
+        return 1
+    fi
 }
 
 #=================================================
